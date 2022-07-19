d4143bb6f84f3708a8aac5044878100af6b0d034
David Visscher <david@davidvisscher.nl>
2022-07-19 17:52:49 +0200

    Day 1 star 2 rusted

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,14 +2,21 @@ use std::fs;
 
 fn main() {
     let values = read_input_file("input");
-    println!("Read values:\n{:?}", values);
-
-    let pairs = create_pairs(values);
-    println!("Created pairs:\n{:?}", pairs);
-
+    let sliding_window_values = sum_sliding_windows(values);
+    let pairs = create_pairs(sliding_window_values);
     let increases = count_increases(pairs);
     println!("{:?}", increases)
 }
+
+/// Creates sums of sliding windows of three values
+fn sum_sliding_windows(input_values: Vec<i32>) -> Vec<i32> {
+    let mut output_values: Vec<i32> = Vec::new();
+    for i in 0..(input_values.len()-2) {
+        output_values.push(input_values[i] + input_values[i+1] + input_values[i+2])
+    }
+    output_values
+}
+
 
 /// Creates pairs from an input vector
 fn create_pairs(input_values: Vec<i32>) -> Vec<[i32; 2]> {
