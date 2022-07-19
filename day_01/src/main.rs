e3f5cb19156c59a765b50d1381d54b6add0b2ccb
David Visscher <david@davidvisscher.nl>
2022-07-19 17:47:18 +0200

    Day 1 in rust!

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,51 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let values = read_input_file("input");
+    println!("Read values:\n{:?}", values);
+
+    let pairs = create_pairs(values);
+    println!("Created pairs:\n{:?}", pairs);
+
+    let increases = count_increases(pairs);
+    println!("{:?}", increases)
+}
+
+fn create_pairs(input_values: Vec<i32>) -> Vec<[i32; 2]> {
+    /// Creates pairs from an input vector
+    let mut output_pairs: Vec<[i32;2]> = Vec::with_capacity(input_values.len());
+
+    for index in 0..(input_values.len()-1) {
+        output_pairs.push(
+            [input_values[index], input_values[index+1]]
+        )
+    }
+    output_pairs
+}
+
+fn count_increases(pairs: Vec<[i32; 2]>) -> i32 {
+    /// counts the amount of increasing pairs in the input vector.
+    let mut increases = 0;
+    for pair in pairs {
+        if pair[1] > pair[0] {
+            increases += 1;
+        }
+    }
+    increases
+}
+
+fn read_input_file(filename: &str) -> Vec<i32> {
+    /// Reads the input file and parses each line into an i32
+    println!("Input filename {}", filename);
+    let contents = fs::read_to_string(filename)
+        .expect("Reading file failed.");
+
+    let value_strings: Vec<&str> = contents.lines().collect();
+    let mut values: Vec<i32> = Vec::with_capacity(value_strings.len());
+
+    for value in value_strings {
+        values.push(value.parse::<i32>().unwrap());
+    }
+
+    values
 }
